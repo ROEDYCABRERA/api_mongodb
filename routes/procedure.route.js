76f639769e61227d8ceae1012ab77dcd20e56de3
ROEDY CABRERA <roedycabrera@gmail.com>
2023-11-28 19:50:21 +0100

    actualizar

--- routes/procedure.route.js
+++ routes/procedure.route.js
@@ -11,7 +11,8 @@ import { requireToken } from "../middlewares/requireToken.js";
 const router = Router();
 
 router.post("/register", bodyProcedureValidator,register);
-router.put("/:id",paramProcedureValidator,bodyProcedureValidator,update);
+//router.put("/:id",paramProcedureValidator,bodyProcedureValidator,update);
+router.put("/update",bodyProcedureValidator,update);
 router.delete("/:id" ,paramProcedureValidator,removeProcedure);
 router.get("/listar",requireToken,getDatos);
 
